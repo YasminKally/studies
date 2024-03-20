dba1a60ef595f8d88f9f553b1d0f712a912179e7
Yasmin Kallyson <146667394+YasminKally@users.noreply.github.com>
2024-03-20 12:54:41 -0300

    weather app project

---
+++ weather-app/js/script.js
@@ -0,0 +1,51 @@
+// Variáveis e seleção de elementos
+const apiKey = 'c023cadecc58b84dcd7327372b3ebf9a';
+
+const cityInput = document.querySelector('#city-input');
+const searchBtn = document.querySelector('#search');
+const form = document.querySelector('.form');
+
+const cityElement = document.querySelector('#city');
+const tempElement = document.querySelector('#temperature span');
+const descElement = document.querySelector('#description');
+const weathericonElement = document.querySelector('#weather-icon');
+const countryElement = document.querySelector('#country');
+const humidityElement = document.querySelector('#humidity span');
+const windElement = document.querySelector('#wind span');
+
+const weatherData = document.querySelector('#weather-data')
+
+// Funções
+const getWeatherData = async (city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    return data;
+};
+
+const showWeatherData = async(city) => {
+    const data  = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weathericonElement.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute('src', `https://flagsapi.com/${data.sys.country}/flat/64.png`);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+
+    weatherData.classList.remove('hide');
+};
+
+// Eventos
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+    
+    showWeatherData(city);
+    cityInput.value = '';
+    cityInput.focus();
+});
