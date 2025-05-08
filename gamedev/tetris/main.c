887df5bc82b3fdb5d3ab437e3dc97d1fef51408a
Yasmin Kallyson <yasminkallyson@gmail.com>
2025-05-08 17:21:51 -0300

    points system, rotation fixed, basic interface

--- gamedev/tetris/main.c
+++ gamedev/tetris/main.c
@@ -15,14 +15,17 @@
 #include "pieces.c"
 
 typedef struct {
+    int points;
     int gameOver;
     int pause;
     int toFetch;
+    int linesToDelete;
+    float deleteTick;
+    float deleteTickSpeed;
     float dropTick;
     float dropTickSpeed;
-    float moveTick;
-    float moveTickSpeed;
     Piece currentPiece;
+    Piece nextPiece;
     unsigned int grid[GRID_C][GRID_L];
 } State;
 
@@ -30,9 +33,11 @@ typedef struct {
 void initGame(State*);
 void updateGame(State*);
 void drawGame(State);
-int checkCollisionY(State*);
-int checkCollisionX(State*, int);
+int checkCollisionY(State);
+int checkCollisionX(State, int);
 void dropPiece(State*);
+Piece rotatePiece(State);
+void deleteLines(State*);
 void dumpGrid(unsigned int [GRID_C][GRID_L]);
 
 int main(void){
@@ -53,51 +58,73 @@ int main(void){
     return 0;
 }
 
+// set initial game state
 void initGame(State *state){
     memset(state->grid, 0, sizeof(unsigned int) * GRID_C * GRID_L);
-    state->dropTick = 0;
-    state->dropTickSpeed = .4f;
-    state->moveTick = 0;
-    state->moveTickSpeed = .2f;
+    state->points = 0;
     state->gameOver = 0;
     state->pause = 0;
     state->toFetch = 1;
+    state->linesToDelete = 0;
+    state->deleteTick = 0;
+    state->deleteTickSpeed = .4f;
+    state->dropTick = 0;
+    state->dropTickSpeed = .4f;
+    state->currentPiece.y = 0;
+    state->nextPiece = fetchPiece(GetRandomValue(1, 7));
 }
 
 void updateGame(State *state){
+    if(state->gameOver){
+        // make sure game is not paused on game over
+        state->pause = 0;
+        if(IsKeyPressed(KEY_R)) initGame(state);
+    }
+    // while game is not over
     if(!state->gameOver){
+        // toggle pause
         if(IsKeyPressed(KEY_ESCAPE) && !state->gameOver) state->pause = !state->pause;
-        if(state->gameOver) state->pause = 0;
-        if(!state->pause){
+        // call the funtion to delete lines if there are lines to delete
+        if(state->linesToDelete) deleteLines(state);
+        // if game is not paused and there aren't any lines to delete
+        if(!state->pause && !state->linesToDelete){
+            // if a new piece should be fetched
             if(state->toFetch){
+                // if the placed piece is already above the grid
                 if(state->currentPiece.y < 0){
                     state->gameOver = 1;
                     return;
                 }
-                state->currentPiece = fetchPiece(GetRandomValue(1, 7));
+                // fetch new piece
+                state->currentPiece = state->nextPiece;
+                state->nextPiece = fetchPiece(GetRandomValue(1, 7));
                 state->toFetch = 0;
                 return;
             }
 
-            state->moveTick += GetFrameTime();
-            if(state->moveTick >= state->moveTickSpeed){
-                if(!checkCollisionX(state, (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) - (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)))){
-                    if(IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) state->currentPiece.x++;
-                    if(IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) state->currentPiece.x--;
-                }
-                state->moveTick = 0;
+            // check horizontal collisions before moving
+            if(!checkCollisionX(*state, (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) - (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)))){
+                if(IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) state->currentPiece.x++;
+                if(IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT)) state->currentPiece.x--;
+            }
+            // rotation
+            if(IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP)){
+                state->currentPiece = rotatePiece(*state);
             }
 
-
+            // update gravity tick
             state->dropTick += GetFrameTime();
+            // change gravity tick when falling faster
             if(IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)){
                 state->dropTickSpeed = .05f;
             } else {
                 state->dropTickSpeed = .4f;
             }
+            // drop piece
             if(state->dropTick >= state->dropTickSpeed){
                 dropPiece(state);
                 state->dropTick = 0;
+                state->linesToDelete = 1;
             }
         }
     }
@@ -105,54 +132,101 @@ void updateGame(State *state){
 
 void drawGame(State state){
     BeginDrawing();
+        // clear frame buffer
         ClearBackground(BLACK);
+        // iterate over all grid blocks and draw each one
         for(int c = 0; c < GRID_C; c++){
             for(int l = 0; l < GRID_L; l++){
+                // draw a collored piece when occupied
                 if(state.grid[c][l]){
                     DrawRectangle((c + 1) * BLOCK_SIZE + 1, (l + 1) * BLOCK_SIZE + 1, BLOCK_SIZE - 2, BLOCK_SIZE - 2, DARKPURPLE);
+                // draw it empty otherwise
                 } else {
                     DrawRectangle((c + 1) * BLOCK_SIZE + 1, (l + 1) * BLOCK_SIZE + 1, BLOCK_SIZE - 2, BLOCK_SIZE - 2, DARKGRAY);
                 }
             }
         }
+        // do not draw the piece when it was just placed
         if(!state.toFetch){
             drawPiece(state.currentPiece);
         }
+        // draw paused overlay
         if(state.pause){
             DrawRectangle(BLOCK_SIZE, BLOCK_SIZE, GRID_C * BLOCK_SIZE, GRID_L * BLOCK_SIZE, (Color){0, 0, 0, 120});
             DrawText("paused", GRID_C * BLOCK_SIZE / 2 + BLOCK_SIZE - MeasureText("paused", 40) / 2, GRID_L * BLOCK_SIZE / 2 + BLOCK_SIZE - 20, 40, RAYWHITE);
         }
+
+        int nextPieceX = (SCREEN_W / 4 - BLOCK_SIZE * 1.5f + SCREEN_W / 2) / BLOCK_SIZE;
+
+        char buffer[256];
+        sprintf(buffer, "points: %d", state.points);
+        DrawText(buffer, SCREEN_W / 2 + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE, RAYWHITE);
+        DrawText("next piece:", SCREEN_W / 2 + BLOCK_SIZE, BLOCK_SIZE * 2, BLOCK_SIZE, RAYWHITE);
+        for(int c = 0; c < 3; c++){
+            for(int l = 0; l < 3; l++){
+                if(state.nextPiece.bricks[c][l] > 1){
+                    DrawRectangle((nextPieceX + c + 1) * BLOCK_SIZE + 1, (3 + l) * BLOCK_SIZE + 1, BLOCK_SIZE - 2, BLOCK_SIZE - 2, PURPLE);
+                } else if(state.nextPiece.bricks[c][l]){
+                    DrawRectangle((nextPieceX + c) * BLOCK_SIZE + 1.5f, (4 + l) * BLOCK_SIZE + 1, BLOCK_SIZE - 2, BLOCK_SIZE - 2, PURPLE);
+                }
+            }
+        }
+
     EndDrawing();
 }
 
-int checkCollisionY(State* state){
+// check collisions with the grid floor and blocks right bellow
+int checkCollisionY(State state){
     for(int c = 0; c < 3; c++){
         for(int l = 0; l < 3; l++){
-            if(state->currentPiece.bricks[c][l]){
-                if(l + state->currentPiece.y + 1 == GRID_L || state->grid[c + state->currentPiece.x][l + state->currentPiece.y + 1]) return 1;
+            // if there is an occupied brick at {c, l}
+            if(state.currentPiece.bricks[c][l]){
+                // and right bellow a block is occupied or the position outbounds the grid
+                if(l + state.currentPiece.y + 1 == GRID_L || state.grid[c + state.currentPiece.x][l + state.currentPiece.y + 1]) return 1;
             }
         }
     }
     return 0;
 }
 
-int checkCollisionX(State* state, int dir){
+// check if a piece is horizontally next to another block or the grid walls
+int checkCollisionX(State state, int dir){
     for(int c = 0; c < 3; c++){
         for(int l = 0; l < 3; l++){
-            if(state->currentPiece.bricks[c][l]){
-                if((c + state->currentPiece.x + 1 == GRID_C || state->grid[c + state->currentPiece.x + 1][l + state->currentPiece.y]) && dir == 1) return 1;
-                if((c + state->currentPiece.x - 1 < 0 || state->grid[c + state->currentPiece.x - 1][l + state->currentPiece.y]) && dir == -1) return 1;
+            // if there is an occupied brick at {c, l}
+            if(state.currentPiece.bricks[c][l]){
+                // rightside collision
+                if((c + state.currentPiece.x + 1 == GRID_C || state.grid[c + state.currentPiece.x + 1][l + state.currentPiece.y]) && dir == 1) return 1;
+                // leftside collision
+                if((c + state.currentPiece.x - 1 < 0 || state.grid[c + state.currentPiece.x - 1][l + state.currentPiece.y]) && dir == -1) return 1;
             }
         }
     }
     return 0;
 }
 
+// check if any brick of a piece is overlapping an occupied grid block
+int checkOverlap(State state, Piece new){
+    for(int c = 0; c < 3; c++){
+        for(int l = 0; l < 3; l++){
+            // if there is an occupied brick at {c, l}
+            if(new.bricks[c][l]){
+                // and also an occupied block at this same position on the grid, offseted by the piece position, return true
+                if(state.grid[c + new.x][l + new.y] || new.x + c < 0 || new.x + c == GRID_C) return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+// drop down a piece
 void dropPiece(State *state){
     unsigned int brick = 0;
 
-    if(!checkCollisionY(state)){
+    // if the no collision is detected, move downwards
+    if(!checkCollisionY(*state)){
         state->currentPiece.y++;
+    // otherwise, place the piece in the grid and set to fetch a new one
     } else {
         for(int c = 0; c < 3; c++){
             for(int l = 0; l < 3; l++){
@@ -166,7 +240,68 @@ void dropPiece(State *state){
     }
 }
 
+// creates a rotated version the current piece.
+// it returns the old one if the rotated version is invalid
+Piece rotatePiece(State state){
+    Piece old = state.currentPiece;
+    Piece new = old;
+    // if current piece is an O, return old
+    if(old.bricks[1][1] == 1) return old;
+
+    // hardcoded rotation
+    new.bricks[0][0] = old.bricks[0][2];
+    new.bricks[0][1] = old.bricks[1][2];
+    new.bricks[0][2] = old.bricks[2][2];
+    new.bricks[1][0] = old.bricks[0][1];
+    new.bricks[1][1] = old.bricks[1][1];
+    new.bricks[1][2] = old.bricks[2][1];
+    new.bricks[2][0] = old.bricks[0][0];
+    new.bricks[2][1] = old.bricks[1][0];
+    new.bricks[2][2] = old.bricks[2][0];
+
+    // check if the rotated version is valid
+    if(checkOverlap(state, new)){
+        return old;
+    }
+
+    return new;
+}
+
+void deleteLines(State *state){
+    static int deletedLines = 0;
+
+    for(int l = 0; l < GRID_L; l++){
+        int count = 0;
+        for(int c = 0; c < GRID_C; c++){
+            if(state->grid[c][l]) count++;
+        }
+        if(count == GRID_C){
+            state->deleteTick += GetFrameTime();
+            if(state->deleteTick >= state->deleteTickSpeed){
+                deletedLines++;
+                for(int dL = l; dL > 0; dL--){
+                    for(int dC = 0; dC < GRID_C; dC++){
+                        if(dL == 0){
+                            state->grid[dC][dL] = 0;
+                        } else {
+                            state->grid[dC][dL] = state->grid[dC][dL - 1];
+                        }
+                    }
+                }
+                state->deleteTick = 0;
+            } else {
+                return;
+            }
+        }
+    }
+    if(deletedLines) state->points += 100 + (deletedLines - 1) * 50;
+    deletedLines = 0;
+    state->linesToDelete = 0;
+}
+
+// dumps current grid state to the stdout
 void dumpGrid(unsigned int grid[GRID_C][GRID_L]){
+    // separation
     printf("----------\n");
     for(int l = 0; l < GRID_L; l++){
         for(int c = 0; c < GRID_C; c++){
