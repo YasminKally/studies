72900fb5d6223473d34b6838ba3fe42d7a2bb59c
Yasmin Kallyson <yasminkallyson@gmail.com>
2025-06-03 21:33:18 -0300

    improved godot beat em up study combat - added more attack animations - added power attack - implemented basic enemy attacks - player receives damage - player and enemy knife animations - player and enemy throw states

--- gamedev/godot/beat_em_up_study/scripts/characters/character.gd
+++ gamedev/godot/beat_em_up_study/scripts/characters/character.gd
@@ -3,8 +3,14 @@ extends CharacterBody2D
 
 const GRAVITY := 600.0
 
+@export var can_respawn: bool
+@export var can_respawn_knife: bool
 @export var damage: int
+@export var damage_power: int
 @export var duration_grounded: float
+@export var duration_between_knife_respawn: int
+@export var flight_speed: float
+@export var has_knife: bool
 @export var jump_intensity: float
 @export var knockback_intensity: float
 @export var knockdown_intensity: float
@@ -13,16 +19,20 @@ const GRAVITY := 600.0
 
 @onready var animation_player := $AnimationPlayer
 @onready var character_sprite := $CharacterSprite
+@onready var collateral_hit_box : Area2D = $CollateralHitBox
 @onready var collision_shape := $CollisionShape2D
 @onready var hit_box := $HitBox
 @onready var hurt_box: HurtBox = $HurtBox
+@onready var knife_sprite: Sprite2D = $KnifeSprite
+@onready var projectile_aim: RayCast2D = $ProjectileAim
 
-enum State {IDLE, WALK, ATTACK, TAKEOFF, JUMP, JUMPKICK, LAND, HURT, FALL, GROUNDED}
+enum State {IDLE, WALK, ATTACK, TAKEOFF, JUMP, JUMPKICK, LAND, HURT, FALL, GROUNDED, DEATH, FLY, PREP_ATTACK, THROW}
 
-var anim_map := {
+var anim_attacks := []
+
+var anim_map: Dictionary = {
 	State.IDLE: "idle",
 	State.WALK: "walk",
-	State.ATTACK: "punch",
 	State.TAKEOFF: "takeoff",
 	State.JUMP: "jump",
 	State.JUMPKICK: "jumpkick",
@@ -30,17 +40,27 @@ var anim_map := {
 	State.HURT: "hurt",
 	State.FALL: "fall",
 	State.GROUNDED: "grounded",
+	State.DEATH: "grounded",
+	State.FLY: "fly",
+	State.PREP_ATTACK: "idle",
+	State.THROW: "throw",
 }
 
+var attack_combo_index := 0
 var state = State.IDLE
+var heading = Vector2.RIGHT
 var height := 0.0
 var height_speed := 0.0
+var is_last_hit_sucessful := false
 var current_health := 0
 var time_since_grounded := Time.get_ticks_msec()
+var time_since_knife_dismiss := Time.get_ticks_msec()
 
 func _ready() -> void:
 	hit_box.area_entered.connect(on_emit_damage.bind())
 	hurt_box.damage_received.connect(on_receive_damage.bind())
+	collateral_hit_box.area_entered.connect(on_emit_collateral_damage.bind())
+	collateral_hit_box.body_entered.connect(on_wall_hit.bind())
 	current_health = max_health
 
 func _process(delta: float) -> void:
@@ -48,10 +68,16 @@ func _process(delta: float) -> void:
 	handle_movement()
 	handle_animations()
 	handle_air_time(delta)
+	handle_prep_attack()
 	handle_grounded()
+	handle_knife_respawns()
+	handle_death(delta)
+	set_heading()
 	flip_sprites()
+	knife_sprite.visible = has_knife
 	character_sprite.position = Vector2.UP * height
-	collision_shape.disabled = state == State.GROUNDED
+	knife_sprite.position = Vector2.UP * height
+	collision_shape.disabled = is_collision_disabled()
 	move_and_slide()
 
 func handle_movement() -> void:
@@ -64,12 +90,29 @@ func handle_movement() -> void:
 func handle_input() -> void:
 	pass
 
+func handle_prep_attack() -> void:
+	pass
+
 func handle_grounded() -> void:
 	if state == State.GROUNDED and (Time.get_ticks_msec() - time_since_grounded > duration_grounded):
-		state = State.LAND
+		if current_health == 0:
+			state = State.DEATH
+		else:
+			state = State.LAND
+
+func handle_knife_respawns() -> void:
+	if can_respawn_knife and not has_knife and (Time.get_ticks_msec() - time_since_knife_dismiss > duration_between_knife_respawn): 
+		has_knife = true
+func handle_death(delta: float) -> void:
+	if state == State.DEATH and not can_respawn:
+		modulate.a -= delta / 2.0
+		if modulate.a <= 0:
+			queue_free()
 
 func handle_animations() -> void:
-	if animation_player.has_animation(anim_map[state]):
+	if state == State.ATTACK:
+		animation_player.play(anim_attacks[attack_combo_index])
+	elif animation_player.has_animation(anim_map[state]):
 		animation_player.play(anim_map[state])
 
 func handle_air_time(delta: float) -> void:
@@ -86,12 +129,19 @@ func handle_air_time(delta: float) -> void:
 		else:
 			height_speed -= GRAVITY * delta
 
+func set_heading() -> void:
+	pass
+
 func flip_sprites() -> void:
-	if velocity.x > 0:
+	if heading == Vector2.RIGHT:
 		character_sprite.flip_h = false
+		knife_sprite.flip_h = false
+		projectile_aim.scale.x = 1
 		hit_box.scale.x = 1
-	elif velocity.x < 0:
+	else:
 		character_sprite.flip_h = true
+		knife_sprite.flip_h = true
+		projectile_aim.scale.x = -1
 		hit_box.scale.x = -1
 
 func can_move() -> bool:
@@ -106,8 +156,18 @@ func can_jump() -> bool:
 func can_jumpkick() -> bool:
 	return state == State.JUMP
 
+func can_get_hurt() -> bool:
+	return [State.IDLE, State.WALK, State.TAKEOFF, State.LAND].has(state)
+
+func is_collision_disabled() -> bool:
+	return [State.GROUNDED, State.DEATH, State.FLY].has(state)
+
 func on_action_complete() -> void:
 	state = State.IDLE
+
+func on_throw_complete() -> void:
+	state = State.IDLE
+	has_knife = false
 
 func on_takeoff_complete() -> void:
 	state = State.JUMP
@@ -117,17 +177,41 @@ func on_land_complete() -> void:
 	state = State.IDLE
 
 func on_receive_damage(amount: int, direction: Vector2, hit_type: HurtBox.HitType) -> void:
-	current_health = clamp(current_health - amount, 0, max_health)
-	if current_health == 0 or hit_type == HurtBox.HitType.KNOCKDOWN:
-		state = State.FALL
-		height_speed = knockdown_intensity
-	else:
-		state = State.HURT
-	velocity = direction * knockback_intensity
+	if can_get_hurt():
+		print(str(amount))
+		if has_knife:
+			has_knife = false
+			time_since_knife_dismiss = Time.get_ticks_msec()
+		current_health = clamp(current_health - amount, 0, max_health)
+		if current_health == 0 or hit_type == HurtBox.HitType.KNOCKDOWN:
+			state = State.FALL
+			height_speed = knockdown_intensity
+			velocity = direction * knockback_intensity
+		elif hit_type == HurtBox.HitType.POWER:
+			state = State.FLY
+			velocity = direction * flight_speed
+		else:
+			state = State.HURT
+			velocity = direction * knockback_intensity
 
 func on_emit_damage(target_hurt_box: HurtBox) -> void:
 	var hit_type = HurtBox.HitType.NORMAL
 	var direction := Vector2.LEFT if target_hurt_box.global_position.x < global_position.x else Vector2.RIGHT
+	var current_damage = damage
 	if state == State.JUMPKICK:
 		hit_type = HurtBox.HitType.KNOCKDOWN
-	target_hurt_box.damage_received.emit(damage, direction, hit_type)
+	if attack_combo_index == anim_attacks.size() - 1:
+		hit_type = HurtBox.HitType.POWER
+		current_damage = damage_power
+	target_hurt_box.damage_received.emit(current_damage, direction, hit_type)
+	is_last_hit_sucessful = true
+
+func on_emit_collateral_damage(target_hurt_box: HurtBox) -> void:
+	if target_hurt_box != hurt_box:
+		var direction := Vector2.LEFT if target_hurt_box.global_position.x < global_position.x else Vector2.RIGHT
+		target_hurt_box.damage_received.emit(0, direction, HurtBox.HitType.KNOCKDOWN)
+
+func on_wall_hit(_wall: AnimatableBody2D) -> void:
+	state = State.FALL
+	height_speed = knockdown_intensity
+	velocity = -velocity / 2.0
--- gamedev/godot/beat_em_up_study/scripts/characters/player.gd
+++ gamedev/godot/beat_em_up_study/scripts/characters/player.gd
@@ -3,15 +3,33 @@ extends Character
 
 @onready var enemy_slots: Array = $EnemySlots.get_children()
 
+func _ready() -> void:
+	super._ready()
+	anim_attacks = ["punch", "punch_alt", "kick", "roundkick"]
+
 func handle_input() -> void:
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * speed
 	if can_attack() and Input.is_action_just_pressed("attack"):
-		state = State.ATTACK
+		if has_knife:
+			state = State.THROW
+		else:
+			state = State.ATTACK
+			if is_last_hit_sucessful:
+				attack_combo_index = (attack_combo_index + 1) % anim_attacks.size()
+				is_last_hit_sucessful = false
+			else: 
+				attack_combo_index = 0
 	if can_jump() and Input.is_action_just_pressed("jump"):
 		state = State.TAKEOFF
 	if can_jumpkick() and Input.is_action_just_pressed("attack"):
 		state = State.JUMPKICK
+
+func set_heading() -> void:
+	if velocity.x > 0:
+		heading = Vector2.RIGHT
+	elif velocity.x < 0:
+		heading = Vector2.LEFT
 
 func reserve_slot(enemy: BasicEnemy) -> EnemySlot:
 	var available_slots := enemy_slots.filter(
